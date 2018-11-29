096487455ef7d661f879f95f06958d1c7221912c
Blue Raptor <ciscoaravena@gmail.com>
2018-11-29 00:22:33 -0700

    js update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,3 +4,8 @@
 //give total of correct and wrong answers at the end
 //have a timer going 
 //have fun
+
+var panel = $("#play");
+$(document).on("click", "#start", function(){
+    game.start();
+});
