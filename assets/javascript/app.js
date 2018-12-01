75ffbfa2635955c278d4891dea38a3c6d219464b
Blue Raptor <ciscoaravena@gmail.com>
2018-12-01 00:11:03 -0700

    big commit, forgot to commit for a long time

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,7 +20,7 @@ var questions = [{
         right: "False"
         },
         {
-        question: "BLANK means good mother lizard",
+        question: "_______ means good mother lizard",
         choices: ["Mososaurus", "Maiasaura", "Megolosaurus", "Microraptor"],
         right: "Maiasaura"
         },
@@ -58,12 +58,12 @@ var questions = [{
                                     question: "True or False, Tyrranosaurus Rex means Tyrant Lizard and King",
                                     choices: ["True", "False"],
                                     right: "True"
-                                    },
-                                    {
-                                        question: "Brachiosaurus was estimated to be..",
-                                        choices: ["Weigh between 30-45 metric tons", "85 feet long", "All of the above"],
-                                        right: "All of the above"
-                                        }];    
+                                    }];
+                                    //{
+                                        // question: "Brachiosaurus was estimated to be..",
+                                        // choices: ["Weigh between 30-45 metric tons", "85 feet long", "All of the above"],
+                                        // right: "All of the above"
+                                        // }];    
 var game = {
     correct: 0,
     incorrect: 0,
@@ -78,16 +78,136 @@ countdown: function(){
     }
 },
 start: function() {
-    timer = setInterval(game.countdown, 1000);
+    timer = setInterval(game.countdown, 2000);
     $('#subcontainer').prepend('<h2>Time Remaining: <span id="counter-number">60</span> Seconds</h2>');
     $("#start").remove();
 
     for (var i = 0; i < questions.length; i++) {
     panel.append('<h2>' + questions[i].question + '</h2>');
     for (var j = 0; j < questions[i].choices.length; j++){
-      panel.append('<input type="radio" name ="question' + '-' + i + '"value="' + questions[i].choices[j] + '">' + questions[i].choices[j]);
+      panel.append('<label>'+'<input type="radio" id="answers" name ="question' + '-' + i + '"value="'  + questions[i].choices[j] + '">' +'</label>' + questions[i].choices[j]);
       }
         }
-        panel.append("<button id='done'>DONE</button>");
-    })
-    
+        panel.append("<button id='done' class='btn btn-outline-light btn-lg'>DONE</button>");
+      //id="start" class="btn btn-outline-light btn-lg"
+    },
+
+    done: function() {
+
+        $.each($("input[name='question-0']:checked"), function() {
+            if ($(this).val() == questions[0].correctAnswer) {
+                console.log(this);
+                  game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-1']:checked"), function() {
+            if ($(this).val() == questions[1].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-2']:checked"), function() {
+            if ($(this).val() == questions[2].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-3']:checked"), function() {
+            if ($(this).val() == questions[3].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-4']:checked"), function() {
+            if ($(this).val() == questions[4].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-5']:checked"), function() {
+            if ($(this).val() == questions[5].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-6']:checked"), function() {
+            if ($(this).val() == questions[6].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-7']:checked"), function() {
+            if ($(this).val() == questions[7].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-8']:checked"), function() {
+            if ($(this).val() == questions[8].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        $.each($("input[name='question-9']:checked"), function() {
+            if ($(this).val() == questions[9].correctAnswer) {
+                console.log(this);
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+
+        });
+        // $.each($("input[name='question-10']:checked"), function() {
+        //     if ($(this).val() == questions[10].correctAnswer) {
+        //         console.log(this);
+        //         game.correct++;
+        //     } else {
+        //         game.incorrect++;
+        //     }
+
+        // });
+
+
+        this.results();
+    },
+
+
+      results:function() {
+          clearInterval(timer);
+
+          $("#subcontainer h2").remove();
+       panel.html("<h2>You're Done!</h2>");
+       panel.append("<h3>You got this many right: " + this.correct + "</h3>");
+       panel.append("<h3>You got this many wrong: " + this.incorrect + "</h3>");
+       panel.append("<h3>You didn't answer this many: " + (questions.length - (this.incorrect + this.correct)) + "</h3>");
+      
+      }
+
+};
